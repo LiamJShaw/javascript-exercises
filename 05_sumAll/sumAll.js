c16e063b7de13c0ec1dfdb45bbe075664ed8b868
Liam Shaw <liamjosephshaw@gmail.com>
2022-02-25 11:33:46 +0000

    All tests passed for ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    
+    let total = 0;
 
+    if (first < 0 || last < 0){
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(first) || !Number.isInteger(last)){
+        return "ERROR";
+    }
+
+    if (first < last){
+        for (let i = first; i < last+1; i++){
+            total += i;
+        }
+    } else {
+        for (let i = last; i < first+1; i++){
+            total += i;
+        }
+    }
+
+    return total;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
